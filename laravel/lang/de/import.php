0c1700804723a60e6692760a7630765c188b6125
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 12:33:10 +0100

    Update de/import- Thu Jan 18 2024 12:33:10 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/import.php
+++ laravel/lang/de/import.php
@@ -5,17 +5,16 @@
  * Release: Working copy
  * Locale: de, German
  * Tagged: import
- * Status: Translated
  * Exported by: patryk
- * Exported at: Fri, 27 Oct 2023 14:02:03 +0200 
+ * Exported at: Thu, 18 Jan 2024 12:29:30 +0100 
  */
 return array (
-  'csv-file-not-found-in-zip' => 'CSV Datei nicht gefunden',
   'failed-to-authorize' => 'Wir können diese Zugangsdaten leider nicht finden.',
-  'failed-to-process-data' => 'Verarbeitung der Daten gescheitert',
-  'invalid-header-value-conjunction' => 'oder',
   'invalid-request' => 'Ungültige Daten',
   'see-status' => 'Aktuellen Importstatus anzeigen',
   'success' => 'Erfolg',
   'unable-to-save-zip' => 'Zip-Datei konnte nicht gespeichert werden',
+  'csv-file-not-found-in-zip' => 'CSV Datei nicht gefunden',
+  'failed-to-process-data' => 'Verarbeitung der Daten gescheitert',
+  'invalid-header-value-conjunction' => 'oder',
 );
