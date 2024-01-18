bcff1c29b26f4b5e047f14914f7bbb51bc4ef8ca
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 11:55:48 +0100

    Update en/errors- Thu Jan 18 2024 11:55:48 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/errors.php
+++ laravel/lang/en/errors.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: errors
  * Exported by: patryk
- * Exported at: Wed, 17 Jan 2024 14:23:33 +0100 
+ * Exported at: Thu, 18 Jan 2024 10:53:58 +0100 
  */
 return array (
   '404-message' => 'This page is not available.',
