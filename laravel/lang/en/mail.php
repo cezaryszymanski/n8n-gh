70cb36956af8dfa610a3a582725db1e3e03b9ac2
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 12:45:43 +0100

    Update en/mail- Thu Jan 18 2024 12:45:43 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/mail.php
+++ laravel/lang/en/mail.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: mail
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:21:12 +0100 
+ * Exported at: Thu, 18 Jan 2024 12:45:36 +0100 
  */
 return array (
   'account-blocked' => 
