f8b2e2eb2a87506c639c61c09a9cbe4d474cd62d
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 12:33:07 +0100

    Update de/errors- Thu Jan 18 2024 12:33:07 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/errors.php
+++ laravel/lang/de/errors.php
@@ -5,14 +5,13 @@
  * Release: Working copy
  * Locale: de, German
  * Tagged: errors
- * Status: Translated
  * Exported by: patryk
- * Exported at: Fri, 27 Oct 2023 14:01:21 +0200 
+ * Exported at: Thu, 18 Jan 2024 12:29:29 +0100 
  */
 return array (
   '404-message' => 'Diese Seite ist nicht verfügbar',
   '404-title' => 'Nicht Verfügbar',
   'contact-administrator' => 'Die maximale Anzahl Benutzer*innen in diesem Account ist erreicht. Bitte kontatkieren Sie den/die Konto-Inhaber*in.',
-  'user-invitation-expired-error' => 'Ihre Einladung zu Artbutler NEXT ist abgelaufen. Bitte kontaktieren sie die Person, die Sie eingeladen hat, damit diese eine erneute Einladung senden kann.',
   'user-invitation-general-error' => 'Ups, da ist etwas mit Ihrer Einladung schiefgelaufen. Bitte kontaktieren Sie die Person, die Sie eingeladen hat.',
+  'user-invitation-expired-error' => 'Ihre Einladung zu Artbutler NEXT ist abgelaufen. Bitte kontaktieren sie die Person, die Sie eingeladen hat, damit diese eine erneute Einladung senden kann.',
 );
