1dde74d4a43decb5c6fd78d9e17f4753a07fee55
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 12:21:17 +0100

    Update en/filter- Thu Jan 18 2024 12:21:17 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/filter.php
+++ laravel/lang/en/filter.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: filter
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 11:09:04 +0100 
+ * Exported at: Thu, 18 Jan 2024 12:21:12 +0100 
  */
 return array (
   'inventory_id' => 'inventory id',
