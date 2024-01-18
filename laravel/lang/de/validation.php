fd844b1ba642d7b917bd7e17cf62a787604a985c
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 12:33:13 +0100

    Update de/validation- Thu Jan 18 2024 12:33:13 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/validation.php
+++ laravel/lang/de/validation.php
@@ -5,9 +5,8 @@
  * Release: Working copy
  * Locale: de, German
  * Tagged: validation
- * Status: Translated
  * Exported by: patryk
- * Exported at: Fri, 27 Oct 2023 14:02:40 +0200 
+ * Exported at: Thu, 18 Jan 2024 12:32:11 +0100 
  */
 return array (
   'accepted' => 'Diese Wert muss bestätigt werden.',
@@ -22,21 +21,13 @@ return array (
   'before_or_equal' => 'Der Wert muss ein Datum vor oder am :date sein.',
   'between' => 
   array (
-    'array' => 'Der Wert muss zwischen :min und :max liegen.',
-    'file' => 'Der Wert muss zwischen :min und :max KB liegen.',
     'numeric' => 'Der Wert muss zwischen :min und :max liegen.',
+    'file' => 'Der Wert muss zwischen :min und :max KB liegen.',
     'string' => 'Der Wert muss zwischen :min und :max Buchstaben liegen',
+    'array' => 'Der Wert muss zwischen :min und :max liegen.',
   ),
   'boolean' => 'Der Wert muss wahr oder falsch sein',
   'confirmed' => 'Die Werte stimmen nicht überein',
-  'country_code' => 'Ungültiger Ländercode',
-  'custom' => 
-  array (
-    'attribute-name' => 
-    array (
-      'rule-name' => 'Eigene Mitteilung',
-    ),
-  ),
   'date' => 'Dies ist kein gültiges Datum',
   'date_equals' => 'Der Wert muss identisch sein mit :date.',
   'date_format' => 'Der Wert entspricht nicht dem Formt :format.',
@@ -52,17 +43,17 @@ return array (
   'filled' => 'Der Wert muss einen Eintrag enthalten.',
   'gt' => 
   array (
-    'array' => 'Der Wert muss mehr als :value Einträge enthalten.',
-    'file' => 'Der Wert muss grösser als :value Kilobyte sein.',
     'numeric' => 'Der Wert muss grösser als :value sein.',
+    'file' => 'Der Wert muss grösser als :value Kilobyte sein.',
     'string' => 'Der Wert muss mehr als :value Zeichen enthalten.',
+    'array' => 'Der Wert muss mehr als :value Einträge enthalten.',
   ),
   'gte' => 
   array (
-    'array' => 'Der Wert muss :value oder mehr Einträge enthalten',
-    'file' => 'Der Wert muss grösser oder gleich :value Kilobytes sein',
     'numeric' => 'Der Wert muss grösser oder gleich :value sein.',
+    'file' => 'Der Wert muss grösser oder gleich :value Kilobytes sein',
     'string' => 'Der Wert muss grösser oder gleich :value Zeichen sein.',
+    'array' => 'Der Wert muss :value oder mehr Einträge enthalten',
   ),
   'image' => 'Dieser Eintrag muss ein Bild sein.',
   'in' => 'Der Wert ist ungültig.',
@@ -74,43 +65,39 @@ return array (
   'json' => 'Der Wert muss ein gültiger JSON String sein',
   'lt' => 
   array (
-    'array' => 'Der Wert muss weniger als :value Einträge enthalten.',
-    'file' => 'Der Wert muss weniger als :value Kilobytes sein.',
     'numeric' => 'Der Wert muss kleiner als :value sein.',
+    'file' => 'Der Wert muss weniger als :value Kilobytes sein.',
     'string' => 'Der Wert muss weniger als :value Zeichen enthalten.',
+    'array' => 'Der Wert muss weniger als :value Einträge enthalten.',
   ),
   'lte' => 
   array (
-    'array' => 'Der Wert darf nicht mehr als :value Einträge enthalten',
-    'file' => 'Der Wert muss kleiner oder gleich :value Kilobytes sein.',
     'numeric' => 'Der Wert muss kleiner oder gleich :value sein.',
+    'file' => 'Der Wert muss kleiner oder gleich :value Kilobytes sein.',
     'string' => 'Der Wert muss weniger oder gleich :value Zeichen enthalten.',
+    'array' => 'Der Wert darf nicht mehr als :value Einträge enthalten',
   ),
   'max' => 
   array (
-    'array' => 'Der Wert darf nicht mehr als :max Einträge enthalten.',
-    'file' => 'Der Wert darf nicht grösser als :max Kilobytes sein.',
     'numeric' => 'Der Wert darf nicht grösser als :max sein.',
+    'file' => 'Der Wert darf nicht grösser als :max Kilobytes sein.',
     'string' => 'Der Wert darf nicht mehr als :max Zeichen enthalten.',
+    'array' => 'Der Wert darf nicht mehr als :max Einträge enthalten.',
   ),
   'mimes' => 'Der Dateityp muss einer der folgenden sein: :values.',
   'mimetypes' => 'Der Dateityp muss einer der folgenden sein:  :values.',
   'min' => 
   array (
-    'array' => 'Der Wert muss mindestens :min Einträge enthalten.',
-    'file' => 'Der Wert muss mindestens :min Kilobytes sein.',
     'numeric' => 'Der Wert muss mindestens :min sein.',
+    'file' => 'Der Wert muss mindestens :min Kilobytes sein.',
     'string' => 'Der Wert muss mindestens :min Zeichen enthalten.',
+    'array' => 'Der Wert muss mindestens :min Einträge enthalten.',
   ),
   'multiple_of' => 'Der Wert muss ein vielfaches von :value sein.',
   'not_in' => 'Der ausgewählte Wert ist ungültig.',
   'not_regex' => 'Das Format ist ungültig.',
   'numeric' => 'Der Wert muss eine Zahl sein.',
-  'password' => 'Das Passwort ist falsch.',
   'present' => 'Der Wert muss vorhanden sein.',
-  'prohibited' => 'Dieser Wert ist nicht erlaubt.',
-  'prohibited_if' => 'Dieser Wert ist nicht erlaubt, wenn :other :value ist.',
-  'prohibited_unless' => 'Dieser Wert ist nicht erlaubt, ausser wenn :other in :values enthalten ist.',
   'regex' => 'Das Format ist ungültig.',
   'required' => 'Dieser Wert ist erforderlich.',
   'required_if' => 'Dieser Wert ist erforderlich, wenn :other :value ist.',
@@ -119,20 +106,32 @@ return array (
   'required_with_all' => 'Dieser Wert ist erforderlich, wenn :values vorhanden sind.',
   'required_without' => 'Dieser Wert ist erforderlich, wenn :values nicht vorhanden ist.',
   'required_without_all' => 'Dieser Wert ist erforderlich, wenn nichts aus :values vorhanden ist.',
+  'prohibited' => 'Dieser Wert ist nicht erlaubt.',
+  'prohibited_if' => 'Dieser Wert ist nicht erlaubt, wenn :other :value ist.',
+  'prohibited_unless' => 'Dieser Wert ist nicht erlaubt, ausser wenn :other in :values enthalten ist.',
   'same' => 'Dieser Wert und :other müssen identisch sein.',
   'size' => 
   array (
-    'array' => 'Dieser Wert muss :size Einträge enthalten.',
-    'file' => 'Dieser Wert muss :size kilobytes sein.',
     'numeric' => 'Dieser Wert muss :size sein',
+    'file' => 'Dieser Wert muss :size kilobytes sein.',
     'string' => 'Dieser Wert muss :size Zeichen enthalten.',
+    'array' => 'Dieser Wert muss :size Einträge enthalten.',
   ),
   'starts_with' => 'Dieser Wert muss mit einem der folgenden beginnen :values.',
   'string' => 'Der Wert muss eine Zeichenkette sein.',
-  'tax_type' => 'Ungültiger Steuertyp',
   'timezone' => 'Der Wert muss eine gültige Zone sein.',
   'unique' => 'Dieser Wert ist bereits vergeben.',
   'uploaded' => 'Dieser Wert konnte nicht hochgeladen werden.',
   'url' => 'Das Feldformat ist ungültig.',
   'uuid' => 'Der Wert muss eine gültige UUID sein.',
+  'custom' => 
+  array (
+    'attribute-name' => 
+    array (
+      'rule-name' => 'Eigene Mitteilung',
+    ),
+  ),
+  'country_code' => 'Ungültiger Ländercode',
+  'tax_type' => 'Ungültiger Steuertyp',
+  'password' => 'Das Passwort ist falsch.',
 );
