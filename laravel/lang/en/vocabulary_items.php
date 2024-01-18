58c50549600df54eb5f8730d2b4e6105239408b4
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 11:55:55 +0100

    Update en/vocabulary_items- Thu Jan 18 2024 11:55:55 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/vocabulary_items.php
+++ laravel/lang/en/vocabulary_items.php
@@ -4,14 +4,33 @@
  * Project: AB.Next
  * Release: Working copy
  * Locale: en, English
- * Tagged: errors
+ * Tagged: vocabulary_items
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 10:53:58 +0100 
+ * Exported at: Thu, 18 Jan 2024 11:09:05 +0100 
  */
 return array (
-  '404-message' => 'This page is not available.',
-  '404-title' => 'Not available',
-  'contact-administrator' => 'Contact your administrator as this account’s user number limit has been reached.',
-  'user-invitation-general-error' => 'There was a problem with your invitation. Please contact the person who invited you to join Artbutler NEXT.',
-  'user-invitation-expired-error' => 'Your invitation has expired. Please contact the person who invited you to join Artbutler NEXT and ask them to resend the invitation.',
+  'work-type' => 
+  array (
+    'drawing' => 'Drawing',
+    'painting' => 'Painting',
+    'sculpture' => 'Sculpture',
+    'video' => 'Video',
+  ),
+  'work-status' => 
+  array (
+    'available' => 'Available',
+    'reserved' => 'Reserved',
+    'sold' => 'Sold',
+  ),
+  'work-location' => 
+  array (
+    'gallery' => 'Gallery',
+    'storage' => 'Storage',
+    'studio' => 'Studio',
+  ),
+  'work-technique' => 
+  array (
+    'oil-on-canvas' => 'Oil on canvas',
+    'acrylic-on-canvas' => 'Acrylic on canvas',
+  ),
 );
