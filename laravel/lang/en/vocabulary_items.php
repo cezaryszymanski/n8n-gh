caeadfe2bb9bb50c5b17bb46bc22c01cbb6160c3
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 11:02:21 +0100

    Update en/- Thu Jan 18 2024 11:02:21 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/vocabulary_items.php
+++ laravel/lang/en/vocabulary_items.php
@@ -4,12 +4,14 @@
  * Project: AB.Next
  * Release: Working copy
  * Locale: en, English
- * Tagged: auth
+ * Tagged: errors
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 10:56:07 +0100 
+ * Exported at: Thu, 18 Jan 2024 10:53:58 +0100 
  */
 return array (
-  'failed' => 'These credentials do not match our records.',
-  'password' => 'The password is incorrect.',
-  'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+  '404-message' => 'This page is not available.',
+  '404-title' => 'Not available',
+  'contact-administrator' => 'Contact your administrator as this account’s user number limit has been reached.',
+  'user-invitation-general-error' => 'There was a problem with your invitation. Please contact the person who invited you to join Artbutler NEXT.',
+  'user-invitation-expired-error' => 'Your invitation has expired. Please contact the person who invited you to join Artbutler NEXT and ask them to resend the invitation.',
 );
