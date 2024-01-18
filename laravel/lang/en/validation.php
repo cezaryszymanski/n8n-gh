0c2663067ecd6219a2af251f245c78725e2a894b
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-18 11:55:54 +0100

    Update en/validation- Thu Jan 18 2024 11:55:54 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/validation.php
+++ laravel/lang/en/validation.php
@@ -5,9 +5,8 @@
  * Release: Working copy
  * Locale: en, English
  * Tagged: validation
- * Status: Translated
  * Exported by: patryk
- * Exported at: Fri, 27 Oct 2023 13:57:18 +0200 
+ * Exported at: Thu, 18 Jan 2024 11:10:29 +0100 
  */
 return array (
   'accepted' => 'The field value must be accepted.',
@@ -22,21 +21,13 @@ return array (
   'before_or_equal' => 'The field value must be a date before or equal to :date.',
   'between' => 
   array (
-    'array' => 'The field value must have between :min and :max items.',
-    'file' => 'The field value must be between :min and :max kilobytes.',
     'numeric' => 'The field value must be between :min and :max.',
+    'file' => 'The field value must be between :min and :max kilobytes.',
     'string' => 'The field value must be between :min and :max characters.',
+    'array' => 'The field value must have between :min and :max items.',
   ),
   'boolean' => 'The field value field must be true or false.',
   'confirmed' => 'The field value confirmation does not match.',
-  'country_code' => 'Invalid country code.',
-  'custom' => 
-  array (
-    'attribute-name' => 
-    array (
-      'rule-name' => 'custom-message',
-    ),
-  ),
   'date' => 'The field value is not a valid date.',
   'date_equals' => 'The field value must be a date equal to :date.',
   'date_format' => 'The field value does not match the format :format.',
@@ -52,17 +43,17 @@ return array (
   'filled' => 'The field must have a value.',
   'gt' => 
   array (
-    'array' => 'The field value must have more than :value items.',
-    'file' => 'The field value must be greater than :value kilobytes.',
     'numeric' => 'The field value must be greater than :value.',
+    'file' => 'The field value must be greater than :value kilobytes.',
     'string' => 'The field value must be greater than :value characters.',
+    'array' => 'The field value must have more than :value items.',
   ),
   'gte' => 
   array (
-    'array' => 'The field value must have :value items or more.',
-    'file' => 'The field value must be greater than or equal :value kilobytes.',
     'numeric' => 'The field value must be greater than or equal :value.',
+    'file' => 'The field value must be greater than or equal :value kilobytes.',
     'string' => 'The field value must be greater than or equal :value characters.',
+    'array' => 'The field value must have :value items or more.',
   ),
   'image' => 'The field value must be an image.',
   'in' => 'The selected field value is invalid.',
@@ -74,43 +65,39 @@ return array (
   'json' => 'The field value must be a valid JSON string.',
   'lt' => 
   array (
-    'array' => 'The field value must have less than :value items.',
-    'file' => 'The field value must be less than :value kilobytes.',
     'numeric' => 'The field value must be less than :value.',
+    'file' => 'The field value must be less than :value kilobytes.',
     'string' => 'The field value must be less than :value characters.',
+    'array' => 'The field value must have less than :value items.',
   ),
   'lte' => 
   array (
-    'array' => 'The field value must not have more than :value items.',
-    'file' => 'The field value must be less than or equal :value kilobytes.',
     'numeric' => 'The field value must be less than or equal :value.',
+    'file' => 'The field value must be less than or equal :value kilobytes.',
     'string' => 'The field value must be less than or equal :value characters.',
+    'array' => 'The field value must not have more than :value items.',
   ),
   'max' => 
   array (
-    'array' => 'The field value must not have more than :max items.',
-    'file' => 'The field value must not be greater than :max kilobytes.',
     'numeric' => 'The field value must not be greater than :max.',
+    'file' => 'The field value must not be greater than :max kilobytes.',
     'string' => 'The field value must not be greater than :max characters.',
+    'array' => 'The field value must not have more than :max items.',
   ),
   'mimes' => 'The field value must be a file of type: :values.',
   'mimetypes' => 'The field value must be a file of type: :values.',
   'min' => 
   array (
-    'array' => 'The field value must have at least :min items.',
-    'file' => 'The field value must be at least :min kilobytes.',
     'numeric' => 'The field value must be at least :min.',
+    'file' => 'The field value must be at least :min kilobytes.',
     'string' => 'The field value must be at least :min characters.',
+    'array' => 'The field value must have at least :min items.',
   ),
   'multiple_of' => 'The field value must be a multiple of :value.',
   'not_in' => 'The selected field value is invalid.',
   'not_regex' => 'The field value format is invalid.',
   'numeric' => 'The field value must be a number.',
-  'password' => 'The password is incorrect.',
   'present' => 'The field value must be present.',
-  'prohibited' => 'The field value field is prohibited.',
-  'prohibited_if' => 'The field value field is prohibited when :other is :value.',
-  'prohibited_unless' => 'The field value field is prohibited unless :other is in :values.',
   'regex' => 'The field value format is invalid.',
   'required' => 'The field value is required.',
   'required_if' => 'The field value field is required when :other is :value.',
@@ -119,20 +106,32 @@ return array (
   'required_with_all' => 'The field value field is required when :values are present.',
   'required_without' => 'The field value field is required when :values is not present.',
   'required_without_all' => 'The field value field is required when none of :values are present.',
+  'prohibited' => 'The field value field is prohibited.',
+  'prohibited_if' => 'The field value field is prohibited when :other is :value.',
+  'prohibited_unless' => 'The field value field is prohibited unless :other is in :values.',
   'same' => 'The field value and :other must match.',
   'size' => 
   array (
-    'array' => 'The field value must contain :size items.',
-    'file' => 'The field value must be :size kilobytes.',
     'numeric' => 'The field value must be :size.',
+    'file' => 'The field value must be :size kilobytes.',
     'string' => 'The field value must be :size characters.',
+    'array' => 'The field value must contain :size items.',
   ),
   'starts_with' => 'The field value must start with one of the following: :values.',
   'string' => 'The field value must be a string.',
-  'tax_type' => 'Invalid tax type.',
   'timezone' => 'The field value must be a valid zone.',
   'unique' => 'The field value has already been taken.',
   'uploaded' => 'The field value failed to upload.',
   'url' => 'The field format is invalid.',
   'uuid' => 'The field value must be a valid UUID.',
+  'custom' => 
+  array (
+    'attribute-name' => 
+    array (
+      'rule-name' => 'custom-message',
+    ),
+  ),
+  'country_code' => 'Invalid country code.',
+  'tax_type' => 'Invalid tax type.',
+  'password' => 'The password is incorrect.',
 );
