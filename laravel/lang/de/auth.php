8fc683d3e2f2c0764279d8f85d71d8895c9512e5
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-19 08:29:29 +0100

    Update de/auth- Fri Jan 19 2024 08:29:28 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/auth.php
+++ laravel/lang/de/auth.php
@@ -6,7 +6,7 @@
  * Locale: de, German
  * Tagged: auth
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:29:29 +0100 
+ * Exported at: Fri, 19 Jan 2024 08:29:16 +0100 
  */
 return array (
   'failed' => 'Diese Zugangsdaten scheinen fehlerhaft.',
