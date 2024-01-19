87946c10cab0d9bea60f0612a3b24fe84d91a6cb
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-19 08:29:24 +0100

    Update en/import- Fri Jan 19 2024 08:29:23 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/import.php
+++ laravel/lang/en/import.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: import
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:21:12 +0100 
+ * Exported at: Fri, 19 Jan 2024 08:29:15 +0100 
  */
 return array (
   'failed-to-authorize' => 'These credentials do not match our records.',
