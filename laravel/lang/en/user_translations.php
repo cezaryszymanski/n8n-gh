835b8188e9a5e8201972fa73043e686fe830fb02
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-19 08:29:26 +0100

    Update en/user_translations- Fri Jan 19 2024 08:29:25 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/user_translations.php
+++ laravel/lang/en/user_translations.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: user_translations
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:21:12 +0100 
+ * Exported at: Fri, 19 Jan 2024 08:29:16 +0100 
  */
 return array (
   'vocabulary_items' => 
