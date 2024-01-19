80c3f8c98927fd598d93c42bc7a15867ed44d8e7
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-19 08:29:33 +0100

    Update de/import- Fri Jan 19 2024 08:29:32 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/import.php
+++ laravel/lang/de/import.php
@@ -6,7 +6,7 @@
  * Locale: de, German
  * Tagged: import
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:29:30 +0100 
+ * Exported at: Fri, 19 Jan 2024 08:29:17 +0100 
  */
 return array (
   'failed-to-authorize' => 'Wir können diese Zugangsdaten leider nicht finden.',
