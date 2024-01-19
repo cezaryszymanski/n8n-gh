83d0b62c58350b0a736a142c1ad0ad554f17e63a
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-19 08:29:19 +0100

    Update en/auth- Fri Jan 19 2024 08:29:18 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/auth.php
+++ laravel/lang/en/auth.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: auth
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:21:11 +0100 
+ * Exported at: Fri, 19 Jan 2024 08:29:14 +0100 
  */
 return array (
   'failed' => 'These credentials do not match our records.',
