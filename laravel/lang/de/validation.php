ce9cd4f6622120202ee72e54111a65dbd33a49ef
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-19 08:29:36 +0100

    Update de/validation- Fri Jan 19 2024 08:29:35 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/validation.php
+++ laravel/lang/de/validation.php
@@ -6,7 +6,7 @@
  * Locale: de, German
  * Tagged: validation
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:32:11 +0100 
+ * Exported at: Fri, 19 Jan 2024 08:29:18 +0100 
  */
 return array (
   'accepted' => 'Diese Wert muss bestätigt werden.',
