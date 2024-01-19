27476bc3faa7b23d6976db15c4f50b1294372925
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-19 08:29:27 +0100

    Update en/validation- Fri Jan 19 2024 08:29:26 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/validation.php
+++ laravel/lang/en/validation.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: validation
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:21:12 +0100 
+ * Exported at: Fri, 19 Jan 2024 08:29:16 +0100 
  */
 return array (
   'accepted' => 'The field value must be accepted.',
