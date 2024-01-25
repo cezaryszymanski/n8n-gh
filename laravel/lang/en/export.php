031d866d09a84164c3c8ad599812b608ed62e52e
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-25 11:43:47 +0100

    Update en/export- Thu Jan 25 2024 11:43:47 GMT+0100 (Central European Standard Time)

--- laravel/lang/en/export.php
+++ laravel/lang/en/export.php
@@ -6,7 +6,7 @@
  * Locale: en, English
  * Tagged: export
  * Exported by: patryk
- * Exported at: Thu, 18 Jan 2024 12:21:11 +0100 
+ * Exported at: Thu, 25 Jan 2024 11:43:40 +0100 
  */
 return array (
   'pdf_export_in_progress' => 'PDF export in progress...',
