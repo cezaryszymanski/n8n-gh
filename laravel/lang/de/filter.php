4124f17bfa46c4439b042ca1a6f3d2e142d3c6a4
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-25 11:43:56 +0100

    Update de/filter- Thu Jan 25 2024 11:43:56 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/filter.php
+++ laravel/lang/de/filter.php
@@ -6,7 +6,7 @@
  * Locale: de, German
  * Tagged: filter
  * Exported by: patryk
- * Exported at: Fri, 19 Jan 2024 08:29:16 +0100 
+ * Exported at: Thu, 25 Jan 2024 11:43:35 +0100 
  */
 return array (
   'inventory_id' => 'Inventarnummer',
