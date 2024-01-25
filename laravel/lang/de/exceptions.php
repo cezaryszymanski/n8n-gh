e14bba9e26516f20fecc1be04508cb36c8d85456
Cezary Szymański <104898426+cezaryszymanski@users.noreply.github.com>
2024-01-25 11:43:54 +0100

    Update de/exceptions- Thu Jan 25 2024 11:43:54 GMT+0100 (Central European Standard Time)

--- laravel/lang/de/exceptions.php
+++ laravel/lang/de/exceptions.php
@@ -6,7 +6,7 @@
  * Locale: de, German
  * Tagged: exceptions
  * Exported by: patryk
- * Exported at: Fri, 19 Jan 2024 08:29:17 +0100 
+ * Exported at: Thu, 25 Jan 2024 11:43:35 +0100 
  */
 return array (
   'cannot-retrieve-works-to-delete' => 'Kann zu löschende Werke nicht abrufen',
